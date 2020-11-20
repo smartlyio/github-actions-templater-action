3713a2cc56f28f5a594b875a5e52de5f78662672
Simon Jagoe <simon.jagoe@smartly.io>
2020-11-20 15:29:31 +0200

    Pass inputs through environment

--- run-templater.bash
+++ run-templater.bash
@@ -1,6 +1,6 @@
-docker pull "${{ inputs.templater-image-name }}"
+docker pull "$DOCKER_IMAGE_NAME"
 docker run --hostname "workflow-update-${REPOSITORY_NAME}" --rm \
     --mount type=bind,src="$(pwd),dst=/devbox/workspace" \
-    --mount type=bind,src="$(pwd)/${{ inputs.templates-location }},dst=/devbox/github-actions-templates" \
-    "${{ inputs.templater-image-name }}" \
+    --mount type=bind,src="$(pwd)/${TEMPLATES_LOCATION},dst=/devbox/github-actions-templates" \
+    "$DOCKER_IMAGE_NAME" \
         render workflows --no-update
