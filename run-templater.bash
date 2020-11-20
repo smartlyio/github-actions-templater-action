e964019e1df1267dfe3a683bb2b15e4df66a3479
Simon Jagoe <simon.jagoe@smartly.io>
2020-11-20 15:04:37 +0200

    Fix shellcheck errors

--- run-templater.bash
+++ run-templater.bash
@@ -1,6 +1,6 @@
-docker pull ${{ inputs.templater-image-name }}
+docker pull "${{ inputs.templater-image-name }}"
 docker run --hostname "workflow-update-${REPOSITORY_NAME}" --rm \
-  --mount type=bind,src="$(pwd),dst=/devbox/workspace" \
-  --mount type=bind,src="$(pwd)/${{ inputs.templates-location }},dst=/devbox/github-actions-templates" \
-  ${{ inputs.templater-image-name }} \
-    render workflows --no-update
+    --mount type=bind,src="$(pwd),dst=/devbox/workspace" \
+    --mount type=bind,src="$(pwd)/${{ inputs.templates-location }},dst=/devbox/github-actions-templates" \
+    "${{ inputs.templater-image-name }}" \
+        render workflows --no-update
