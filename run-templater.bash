97ba039eb25161c860b87de6c5f53a07dc3e5d84
Simon Jagoe <simon.jagoe@smartly.io>
2021-01-29 19:40:09 +0200

    Add option to render with --local

--- run-templater.bash
+++ run-templater.bash
@@ -1,7 +1,12 @@
 REPOSITORY_NAME="$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}' | sed -e "s/:refs//")"
+render_arg="--no-update"
+local_render="${{ inputs.local-render }}"
+if [[ "${local_render}" == "true" ]]; then
+    render_arg="--local"
+fi
 docker pull "${{ inputs.templater-image-name }}"
 docker run --hostname "workflow-update-${REPOSITORY_NAME}" --rm \
     --mount type=bind,src="$(pwd),dst=/devbox/workspace" \
     --mount type=bind,src="$(pwd)/${{ inputs.templates-location }},dst=/devbox/github-actions-templates" \
     "${{ inputs.templater-image-name }}" \
-        render workflows --no-update
+        render workflows "$render_arg"
