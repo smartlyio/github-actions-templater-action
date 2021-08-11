fe79eccd71dab81814e3ff1156d9ef3309a54e42
Simon Jagoe <simon.jagoe@smartly.io>
2021-08-11 11:59:10 +0300

    Fix disable script

--- disable-workflows.bash
+++ disable-workflows.bash
@@ -1,4 +1,4 @@
-mapfile -t WORKFLOWS < <(find .github/workflows -name "${INPUT_DISABLE_WORKFLOWS_PREFIX}*")
+mapfile -t WORKFLOWS < <(find .github/workflows -type f -name "${INPUT_DISABLE_WORKFLOWS_PREFIX}*.yml")
 
 for workflow in "${WORKFLOWS[@]}"; do
     echo "Disabling $workflow"
