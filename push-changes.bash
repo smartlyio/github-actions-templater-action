b97b9800eae4bcfc32bf32c5eec017ad29a391fd
Simon Jagoe <simon.jagoe@smartly.io>
2021-08-13 09:14:00 +0300

    Revert "Merge pull request #15 from smartlyio/only-make-pr-if-changes"
    This reverts commit b155ff2e6cfdeac94c188ff0df9586c6dacb1016, reversing
    changes made to c2aaca519ae9c25c180c1781684d706ca8a9b79b.

--- push-changes.bash
+++ push-changes.bash
@@ -47,7 +47,7 @@ if [ -n "$git_changes" ]; then
         echo "Close the PR as there are no longer relevant changes"
         gh pr close "$BRANCH_NAME"
       fi
-    elif [[ "$has_changes" == "true" ]]; then
+    else
       echo "Make sure the labels exist"
       if [ -n "$PR_LABEL" ]; then
         if ! gh api "/repos/smartlyio/${REPOSITORY_NAME}/labels/${PR_LABEL}"; then
